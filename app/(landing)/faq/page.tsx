547ea02b1a7a59dbbf2ec50adfab6284d25128bb
SebastianEscudero <sebastin.escudero@uc.cl>
2024-04-17 20:31:20 -0400

    arreglos

--- app/(landing)/faq/page.tsx
+++ app/(landing)/faq/page.tsx
@@ -6,7 +6,7 @@ const AboutUs = () => {
             <h1 className="text-center text-7xl font-cursive mb-12">Preguntas Frecuentes</h1>
             <Accordion type="single" collapsible className="sm:text-3xl text-2xl xl:mx-[10%] lg:mx-[7%] md:mx-[5%] mx-[5%] space-y-9">
                 <AccordionItem value="item-1" className="px-4">
-                    <AccordionTrigger className="font-semibold border-b border-black">¿Donde estan ubicados?</AccordionTrigger>
+                    <AccordionTrigger className="font-semibold border-b border-black">¿Donde están ubicados?</AccordionTrigger>
                     <AccordionContent className="flex flex-col w-full gap-1 text-2xl">
                         <p>Diseñoprintchicureo es tienda online, no es tienda fisica.</p>
                     </AccordionContent>
@@ -14,13 +14,13 @@ const AboutUs = () => {
                 <AccordionItem value="item-2" className="px-4">
                     <AccordionTrigger className="font-semibold border-b border-black">¿Donde entregan?</AccordionTrigger>
                     <AccordionContent className="flex flex-col w-full gap-1 text-2xl">
-                        <p>Las entregas son por delivery a domicilio valor de la entrega desde los $3.000.</p>
+                        <p>Las entregas son por deliver y a domicilio valor de la entrega desde los $3.000.</p>
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="item-3" className="px-4">
                     <AccordionTrigger className="font-semibold border-b border-black">¿Cómo envío los archivos?</AccordionTrigger>
                     <AccordionContent className="flex flex-col w-full gap-1 text-2xl">
-                        <p>Los archivos los puedes enviar en pdf, Ai, Jpg, png. Por mail <a className="text-blue-700" href="mailto:printchicureo@gmail.com">printchicureo@gmail.com</a> </p>
+                        <p>Los archivos los puedes enviar en Pdf, Adobe Ilustrator, Jpg, Png. Por mail <a className="text-blue-700" href="mailto:printchicureo@gmail.com">printchicureo@gmail.com</a> </p>
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="item-4" className="px-4">
